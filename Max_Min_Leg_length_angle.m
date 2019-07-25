f359b697f24b0537d1e036882ea9bf84e87c67af
Sanha Lee <47994496+sanhalee17@users.noreply.github.com>
2019-07-25 10:48:00 -0400

    Add files via upload
    This MATLAB code calculates minimum and maximum length and angle of OpenDog Leg

---
+++ Max_Min_Leg_length_angle.m
@@ -0,0 +1,27 @@
+%Maximum and minimum angla and length calculation
+
+%OpenDog Project
+%7/24/2019
+
+%Sanha Lee
+
+%Defining constant
+%unit: in
+
+x_min = 6.875;
+y_min = 16.625;
+
+x_max = 10;
+y_max = 25.75;
+
+%distance calculation
+min_length = sqrt(x_min ^2 + y_min^2) 
+max_length = sqrt(x_max ^2 + y_max^2)
+
+%extended leg angle calculation
+max_angle_E = atan(11.5/13.75)*(180/pi) %degree
+
+
+%contracted leg angle culation
+max_angle_C = atan(1.25/17.875) *(180/pi)
+min_angle_C = atan(17.25/5.875) *(180/pi)
